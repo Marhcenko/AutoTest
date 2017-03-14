87f4c975e01c36145d2cf094f3c2abf48f7f0a91
shyrik <qamarchenko@gmail.com>
2017-03-14 19:10:45 +0200

    Second Commit

---
+++ src/test/java/Test1.java
@@ -0,0 +1,15 @@
+import org.junit.Test;
+
+/**
+ * Created by User on 09.03.2017.
+ */
+public class Test1 {
+    @Test
+    public void firstTest(){
+        System.out.println("first test");
+    }
+    @Test
+    public void Secondtest(){
+        System.out.println("Second test");
+    }
+}
